ce359cdfbfd23cc808dbea146e319db399b34e09
Osman Turan <turan@anwalt.de>
2024-09-24 16:04:42 +0200

    init search app

---
+++ packages/search-app/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { UiTheMessage } from '#components'
+
+const theMessage = ref<InstanceType<typeof UiTheMessage> | null>()
+
+onMounted(() => {
+  console.log('theMessage', theMessage.value)
+})
+
+const config = useRuntimeConfig()
+</script>
+
+<template>
+  <div>
+    {{ config.public.apiBase }}
+    <p>search</p>
+    <p>search</p>
+    <p>search</p>
+    <UiTheMessage ref="theMessage" />
+  </div>
+</template>
